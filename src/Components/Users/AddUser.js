13e63422e727bd1b7022017ff494f0c1bf179839
varun Pratap <varun.singh01@indiamart.com>
2022-04-24 00:08:47 +0530

    updated

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
-import classes from './AddUser.module.css'
+import classes from "./AddUser.module.css";
+import Button from "../UI/Button";
 function AddUser() {
+  const [user, setuser] = useState("");
+  const [age, setage] = useState("");
+
   const submitHandler = (event) => {
     event.preventDefault();
+      if (user.trim().length === 0 || age.trim().length === 0) {
+          return;
+      }
+      if (+age < 1) {
+          return;
+      }
+      console.log(user, age);
+      setage('');
+      setuser('');
+  };
+
+  const userCollector = (event) => {
+    setuser(event.target.value);
+  };
+
+  const ageCollector = (event) => {
+    setage(event.target.value);
   };
   return (
     <Card className={classes.input}>
       <form onSubmit={submitHandler}>
         <label htmlFor="username">User Name</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" value={user} onChange={userCollector} />
         <label htmlFor="age">Age(Years)</label>
-        <input id="age" type="number" />
-        <button type="submit">Submit</button>
+              <input id="age" type="number" value={age } onChange={ageCollector} />
+        <Button type='submit'>Add User</Button>
       </form>
     </Card>
   );
